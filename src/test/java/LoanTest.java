acd83d23006547c3d779e30105d4fc4ce5858a7b
jyoyu <jockegidlund@gmail.com>
2024-12-09 12:04:05 +0100

    Updated LoanTest so the test checks for the proper due date. Rebuilt project.

--- src/test/java/LoanTest.java
+++ src/test/java/LoanTest.java
@@ -31,7 +31,7 @@ public class LoanTest {
         Loan loan = new Loan(1, customer, book1);
 
         assertNotNull(loan.getDueDate(), "Date is assigned on loan construction.");
-        assertTrue(LocalDate.now().isBefore(loan.getDueDate()),
+        assertTrue(loan.getDueDate().isEqual(LocalDate.now().plusDays(14)),
                     "Added date is after today.");
     }
 }
